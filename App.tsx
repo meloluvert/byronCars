9048c6519694e9b62e0385ff93a708ca2787ad71
Lucas Melo <lucasmelo8328@gmail.com>
2026-02-28 16:40:26 -0300

    feat: setup do app

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
-
+import { Text } from 'react-native';
 import './global.css';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
+
+      <Text className='text-blue-500' > byronCars</Text>
     </SafeAreaProvider>
   );
 }
